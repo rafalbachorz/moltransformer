2b6c0b204e113e5c009267bccf253a515bcc24b2
Rafal Bachorz <rafal@bachorz.eu>
2022-07-31 05:53:15 +0200

    working example

---
+++ moltransformer/paths/__init__.py
@@ -0,0 +1,1 @@
+from .paths import PIPELINES_DIR, MODELS_DIR
---
+++ moltransformer/paths/paths.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+root = Path(__file__).parent.parent.parent
+
+PIPELINES_DIR = root/"pipelines"
+MODELS_DIR = root/"models"
